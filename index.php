bfce4ca6b4765c0d8647cf46dbe36e76ed0e8d2d
Kai Matolat <kaimatolat@hotmail.com>
2019-01-11 22:54:24 +1000

    Changed index.php

--- index.php
+++ index.php
@@ -1,14 +1,16 @@
-<!DOCTYPE html>
-<html>
-<head>
-    <meta charset="utf-8" />
-    <meta http-equiv="X-UA-Compatible" content="IE=edge">
-    <title>PHP Project</title>
-    <meta name="viewport" content="width=device-width, initial-scale=1">
-    <link rel="stylesheet" type="text/css" media="screen" href="assets/css/main.css" />
-    <script src="assets/js/main.js"></script>
-</head>
-<body>
-    
-</body>
-</html>
+<?php
+
+require 'assets/php/config.php';
+
+?>
+<?php
+
+include('assets/php/header.php');
+
+?>
+
+<?php
+
+include('assets/php/footer.php');
+
+?>
